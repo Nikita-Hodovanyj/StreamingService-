05bdc08e9374283064a65a024737d15dd49a5f50
Nikita Hodovanyj <nikitahodovanyj@MacBook-Pro-Nikita.local>
2025-04-25 15:13:58 +0300

    push project

---
+++ films_app/urls.py
@@ -0,0 +1,8 @@
+from .views import *
+from django.urls import path
+
+urlpatterns = [
+    path('all/', render_all_films, name = 'all_films'),
+    path('add-to-favourites/<film_pk>', add_to_favourite, name = 'add_to_favourite'),
+    path('favourites/', render_favourite_films, name= 'favourites')
+]
